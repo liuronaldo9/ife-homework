eed121b652a14b02e4004544cee32d9ff4808535
xue1206 <1085523029@qq.com>
2017-05-08 15:18:54 +0800

    暗杀

--- cslg/cslgIE/js/main.js
+++ cslg/cslgIE/js/main.js
@@ -19,13 +19,18 @@ $(document).ready(function () {
     $("#navbar>ul>li>a").css("font-size", fontsize+"px");
     */
     //myFocus插件
+    //计算图片大小
+    var imgW = $("#carousel").width();
+    var imgH = imgW * 2 / 3;
+    $("#carousel img").css("height", imgH + "px");  //改变图片高度
+    console.log(imgW);
     myFocus.set({
         id: 'carousel',//焦点图盒子ID
         pattern: 'mF_YSlider',//风格应用的名称
         time: 5,//切换时间间隔(秒)
         trigger: 'mouseover',//触发切换模式:'click'(点击)/'mouseover'(悬停)
-        width: 600,//设置图片区域宽度(像素)
-        height: 400,//设置图片区域高度(像素)
+        width: imgW,//设置图片区域宽度(像素)
+        height: imgH,//设置图片区域高度(像素)
         txtHeight: 'default'//文字层高度设置(像素),'default'为默认高度，0为隐藏
     });
 
