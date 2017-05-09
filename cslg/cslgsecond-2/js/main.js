d448b08994eea70143b8c4046b03ca6f01941cf8
xue1206 <1085523029@qq.com>
2017-05-09 13:21:31 +0800

    f

---
+++ cslg/cslgsecond-2/js/main.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function () {
+    //下拉菜单滑动
+    $("#navbar>ul>li:not(:first)").hover(function () {
+        $(this).find("ul").stop().slideToggle("fast");
+
+    });
+    //手机的下拉按钮
+    $("#toggle-bar").click(function () {
+        $("#navbar").toggleClass("navbar-overflow");
+        $("#toggle-bar2").fadeToggle("fast");
+    });
+    //last li border-bottom：none 
+    $(".title-list").find("li").last().css("border-bottom", "none");
+
+});
